e0eec1dee7b03dd67238e59cc79810163c7b8e59
Riyaz <sayyadriyaz0414@gmail.com>
2024-04-26 12:04:14 +0530

    JS added

--- AppCustomization.js
+++ AppCustomization.js
@@ -8,9 +8,20 @@ class AppCustomizations {
     firstFunction(){
       alert("Focus is removed from custom field.")
    }
+
+   generateQuote(){
+      fetch("https://type.fit/api/quotes")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        rand = Math.floor(Math.random() * 10)
+        return data[ rand ]
+      });
+   }
+  
   }
- 
- 
- 
+
   export default AppCustomizations;
  
